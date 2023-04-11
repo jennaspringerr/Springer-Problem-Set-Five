998cc0dc22fd5c97b468a2ca1af1def88adb6468
Jenna <jjs489@cornell.edu>
2023-04-10 23:27:19 -0400

    I tried.

---
+++ Submit.jl
@@ -0,0 +1,5 @@
+# TODO: Fill me in with your program to compute the uknown model parameters from the data
+include("Include.jl")
+
+# Loading the CSV file
+path_to_data_file = joinpath(_PATH_TO_DATA, "PS5-dataset-w-repeats.csv")
